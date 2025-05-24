637e5f1acd203497b85ed32517f53114020f5ccf
Luis Guisado <lguisadom@gmail.com>
2025-05-24 01:01:52 -0500

    Update main.tf

--- terraform/dynamodb/main.tf
+++ terraform/dynamodb/main.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "dim_clienteg1" {
-  name           = "${var.dynamodb_table_name}"
+  name           = "${var.group}-${var.env}-${var.dynamodb_table_name}-${var.prefix}"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "Id_Cliente"
 
